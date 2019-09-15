4134272c39b350a87af17bb9b66f9aefcc9794e7
Thiago Vaz Dias <1681936+tvdias@users.noreply.github.com>
2019-09-15 19:46:13 +0100

    Apply linter changes

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import * as core from '@actions/core';
-import * as github from '@actions/github';
+import * as core from "@actions/core";
+import * as github from "@actions/github";
 
 async function run() {
   try {
-    const token = core.getInput('repo-token', {required: true});
-    const tag = core.getInput('tag', {required: true});
-    const sha = core.getInput('commit-sha', {required: false}) || github.context.sha;
+    const token = core.getInput("repo-token", {required: true});
+    const tag = core.getInput("tag", {required: true});
+    const sha = core.getInput("commit-sha", {required: false}) || github.context.sha;
 
     const client = new github.GitHub(token);
 
@@ -16,7 +16,6 @@ async function run() {
       ref: `refs/tags/${tag}`,
       sha: sha
     });
-
   } catch (error) {
     core.error(error);
     core.setFailed(error.message);
