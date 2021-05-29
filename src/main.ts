6c9630bb6a4574b82d66da2ad3ad4f636199a975
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2021-05-29 14:45:03 +1000

    tidy

--- src/main.ts
+++ src/main.ts
@@ -12,32 +12,6 @@ async function run() {
 
     core.debug(`tagging #${sha} with tag ${tag}`);
 
-    const params = {
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo,
-      ref: `tags/${tag}`
-    };
-
-    core.debug(JSON.stringify(params));
-
-    const exists = await client
-      .request(`GET /repos/:owner/:repo/git/:ref`, params)
-      .catch(_ => false);
-
-    core.debug(exists ? "exists" : "not exists");
-
-    core.debug(`get ref`);
-
-    var result = await client.git.getRef(params);
-
-    core.debug(`result ${result}`);
-
-    // await client.git.deleteRef({
-    //   owner: github.context.repo.owner,
-    //   repo: github.context.repo.repo,
-    //   ref: `refs/tags/${tag}`
-    // });
-
     await client.git.updateRef({
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
